fae7ad7ea8ca94e692ab8e9adf8a25747fb33b4c
MeyankGarg <109937367+MeyankGarg@users.noreply.github.com>
2023-06-26 17:22:53 +0530

    Update streamlit_app.py
    only first line

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import  pickle
 import pandas
 
 
-# movies_list=pickle.load(open('movies_list.pkl','rb'))
+movies_list=pickle.load(open('movies_list.pkl','rb'))
 # df=pickle.load(open('df.pkl','rb'))
 # vector=pickle.load(open('vector.pkl','rb'))
 # similarity=pickle.load(open('similarity.pkl','rb'))
