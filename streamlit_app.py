495af65d4f101494eeeac316c2db27876b9fbc84
MeyankGarg <109937367+MeyankGarg@users.noreply.github.com>
2023-06-26 17:22:20 +0530

    Update streamlit_app.py
    only title

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import  pickle
 import pandas
 
 
-movies_list=pickle.load(open('movies_list.pkl','rb'))
-df=pickle.load(open('df.pkl','rb'))
-vector=pickle.load(open('vector.pkl','rb'))
-similarity=pickle.load(open('similarity.pkl','rb'))
+# movies_list=pickle.load(open('movies_list.pkl','rb'))
+# df=pickle.load(open('df.pkl','rb'))
+# vector=pickle.load(open('vector.pkl','rb'))
+# similarity=pickle.load(open('similarity.pkl','rb'))
 
 
 
