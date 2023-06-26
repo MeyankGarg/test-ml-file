535290fed42c48c235616612c208350699d3e922
MeyankGarg <109937367+MeyankGarg@users.noreply.github.com>
2023-06-26 16:51:10 +0530

    Update streamlit_app.py
    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,21 @@
+import streamlit as st
+import  pickle
+import pandas
+
+
+movies_list=pickle.load(open('movies_list.pkl','rb'))
+df=pickle.load(open('df.pkl','rb'))
+vector=pickle.load(open('vector.pkl','rb'))
+similarity=pickle.load(open('similarity.pkl','rb'))
+
+
+
+
+
+import pickle
+
+st.title('Movie Recommendation System : Collaborative Filter')
+
+
+
 
